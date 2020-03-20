fa65707b042206ce2e19731516d5fa6534af8de3
nokazn <baleariclemon@gmail.com>
2020-03-20 18:31:10 +0000

    add type annotation

--- pages/login/callback.vue
+++ pages/login/callback.vue
@@ -4,13 +4,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Spotify } from '@/types';
 
 export default Vue.extend({
   async fetch({
     query, $axios, store, redirect,
   }): Promise<void> {
     const { code } = query;
-    const { data } = await $axios({
+    const { data }: { data: Spotify.Auth.TokenResponseData | null } = await $axios({
       method: 'post',
       url: '/api/auth/callback',
       params: {
