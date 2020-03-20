00c4d8010058d92bc6846253213bcc0aa82f6c89
nokazn <baleariclemon@gmail.com>
2020-03-20 05:49:09 +0000

    add callback & account pages

---
+++ pages/account.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Account Page</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+});
+</script>
---
+++ pages/login/callback.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>loading...</span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  middleware({ query, $axios, redirect }): void {
+    const { code } = query;
+
+    $axios({
+      method: 'post',
+      url: '/api/auth/callback',
+      params: {
+        code,
+      },
+    })
+      .then(() => {
+        redirect('/');
+      })
+      .catch((e) => console.error(e));
+  },
+});
+</script>
