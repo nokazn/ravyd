aa20c5bca63cb2a05546e4f0a0d65b10caf29245
nokazn <baleariclemon@gmail.com>
2020-04-17 12:33:28 +0000

    new releases

---
+++ components/parts/card/ReleaseCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-card
+    hover
+    ripple
+    tile
+    width="200"
+    :class="$style.ReleaseCard">
+    <div :class="$style.ReleaseCard__container">
+      <v-hover #default="{ hover }">
+        <v-img
+          :src="src"
+          :alt="alt"
+          :height="height"
+          :width="width"
+          :aspect-ratio="1">
+          <release-card-mask v-if="hover" />
+        </v-img>
+      </v-hover>
+
+      <v-card-title :class="$style.ReleaseCard__title">
+        {{ releaseName }}
+      </v-card-title>
+
+      <v-card-subtitle :class="$style.ReleaseCard__subtitle">
+        {{ artistName }}
+      </v-card-subtitle>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ReleaseCardMask from '@/components/parts/card/ReleaseCardMask.vue';
+
+export default Vue.extend({
+  components: {
+    ReleaseCardMask,
+  },
+
+  props: {
+    releaseName: {
+      type: String,
+      required: true,
+    },
+    artistName: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      default: 160,
+    },
+    width: {
+      type: Number,
+      default: 160,
+    },
+  },
+
+  computed: {
+    alt() {
+      return `${this.releaseName} - ${this.artistName}`;
+    },
+  },
+});
+</script>
+
+<style lang="scss" module>
+.ReleaseCard {
+  &__container {
+    display: flex;
+    flex-direction: column;
+  }
+  &__title {
+    font-size: 0.78rem;
+    padding: 12px 8px;
+    line-height: 1rem;
+  }
+  &__subtitle {
+    font-size: 0.7rem;
+    padding: 8px;
+    line-height: 1rem;
+    margin-top: -8px!important;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,64 @@
 <template>
-  <main class="root-page" />
+  <main :class="$style.RootPage">
+    <h2 :class="$style.RootPage__title">
+      今週の新譜
+    </h2>
+    <div :class="$style.RootPage__releaseCardContainer">
+      <release-card
+        v-for="release in newReleaseList"
+        :key="release.name"
+        v-bind="release" />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import ReleaseCard from '@/components/parts/card/ReleaseCard.vue';
+
+export type ReleaseCardInfo = {
+  releaseName: string
+  artistName: string
+  src: string
+}
 
 export default Vue.extend({
+  components: {
+    ReleaseCard,
+  },
+
   async fetch({ app }) {
     await app.$dispatch('browse/getNewReleases');
   },
+
+  computed: {
+    newReleaseList(): ReleaseCardInfo[] {
+      const releases = this.$state().browse.newReleases?.items
+        .map((album) => ({
+          releaseName: album.name,
+          artistName: album.artists[0].name,
+          src: album.images[0].url,
+        }));
+
+      return releases ?? [];
+    },
+  },
 });
 </script>
+
+<style lang="scss" module>
+.RootPage {
+  margin: 16px 32px;
+  &__title {
+    margin-bottom: 16px;
+  }
+  &__releaseCardContainer {
+    display: flex;
+    overflow-x: auto;
+    margin: 12px -32px;
+    & > *:not(:last-child) {
+      margin-right: 16px;
+    }
+  }
+}
+</style>
