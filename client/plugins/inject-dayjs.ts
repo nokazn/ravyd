697a042791c81a8d14556977c1b9ca072777611b
nokazn <baleariclemon@gmail.com>
2020-05-10 23:49:24 +0900

    dayjs locale settings

--- client/plugins/inject-dayjs.ts
+++ client/plugins/inject-dayjs.ts
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
 import { Plugin } from '@nuxt/types';
 
 const injectDayjs: Plugin = (_context, inject) => {
+  dayjs.locale('ja');
   inject('dayjs', dayjs);
 };
 
