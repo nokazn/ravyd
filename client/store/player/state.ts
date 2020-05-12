7d4dcb4f00f33a9f60ffae98cc68451761d80707
nokazn <baleariclemon@gmail.com>
2020-05-12 12:28:27 +0900

    add RootState in player store

--- client/store/player/state.ts
+++ client/store/player/state.ts
@@ -25,6 +25,28 @@ export type PlayerState = {
   volume: number
 }
 
+export type RootState = {
+  'player/deviceId': PlayerState['deviceId']
+  'player/activeDeviceList': PlayerState['activeDeviceList']
+  'player/artWorkSrc': PlayerState['artWorkSrc']
+  'player/trackName': PlayerState['trackName']
+  'player/trackId': PlayerState['trackId']
+  'player/trackUri': PlayerState['trackUri']
+  'player/albumUri': PlayerState['albumUri']
+  'player/artistList': PlayerState['artistList']
+  'player/nextTrackList': PlayerState['nextTrackList']
+  'player/previousTrackList': PlayerState['previousTrackList']
+  'player/recentlyPlayed': PlayerState['recentlyPlayed']
+  'player/isSavedTrack': PlayerState['isSavedTrack']
+  'player/isPlaying': PlayerState['isPlaying']
+  'player/position': PlayerState['position']
+  'player/duration': PlayerState['duration']
+  'player/isShuffled': PlayerState['isShuffled']
+  'player/repeatMode': PlayerState['repeatMode']
+  'player/disallowList': PlayerState['disallowList']
+  'player/volume': PlayerState['volume']
+}
+
 const state = (): PlayerState => ({
   deviceId: null,
   activeDeviceList: [],
