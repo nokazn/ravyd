4a5bf86965f7fcbb3b30ace573398049abcb0071
nokazn <baleariclemon@gmail.com>
2020-05-26 19:27:13 +0900

    scroll button

--- client/components/parts/wrapper/ScrollableCardsWrapper.vue
+++ client/components/parts/wrapper/ScrollableCardsWrapper.vue
@@ -4,13 +4,14 @@
       <div :class="$style.CardsWrapper">
         <transition name="fade">
           <v-btn
-            v-show="hover"
+            v-show="hover && !isScrollOnLeftEdge"
             icon
             large
             :class="[
               $style.CardsWrapper__icon,
               $style['CardsWrapper__icon--left']
-            ]">
+            ]"
+            @click="onLeftButtonClicked">
             <v-icon :size="48">
               mdi-chevron-left
             </v-icon>
@@ -19,14 +20,15 @@
 
         <transition name="fade">
           <v-btn
-            v-show="hover"
+            v-show="hover && !isScrollOnRightEdge"
             icon
             absolute
             large
             :class="[
               $style.CardsWrapper__icon,
               $style['CardsWrapper__icon--right']
-            ]">
+            ]"
+            @click="onRightButtonClicked">
             <v-icon :size="48">
               mdi-chevron-right
             </v-icon>
@@ -34,6 +36,7 @@
         </transition>
 
         <div
+          ref="cardsWrapper"
           :class="[
             $style.CardsWrapper__container,
             'g-no-scroll-bar'
@@ -47,8 +50,114 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import debounce from 'lodash/debounce';
 
-export default Vue.extend({});
+export { ReleaseCardInfo } from '~/components/containers/card/ReleaseCard.vue';
+export { ArtistCardInfo } from '~/components/containers/card/ArtistCard.vue';
+
+const CARDS_WRAPPER_MARGIN = 24;
+
+export type Data = {
+  cardList: {
+    isVisible: boolean
+    element: HTMLDivElement
+  }[],
+  scrollMax: number
+  scrollLeft: number
+}
+
+export type CardType = 'release' | 'artist'
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      cardList: [],
+      scrollMax: 0,
+      scrollLeft: 0,
+    };
+  },
+
+  computed: {
+    isScrollOnLeftEdge(): boolean {
+      return this.scrollLeft === 0;
+    },
+    isScrollOnRightEdge(): boolean {
+      return this.scrollLeft === this.scrollMax;
+    },
+  },
+
+  mounted() {
+    const cardsWrapperRef = this.$refs.cardsWrapper as HTMLDivElement;
+    if (cardsWrapperRef == null) {
+      console.warn('Cannot find the ref of "cardsWrapperRef".');
+      return;
+    }
+
+    this.scrollMax = cardsWrapperRef.scrollWidth - cardsWrapperRef.clientWidth;
+    this.scrollLeft = cardsWrapperRef.scrollLeft;
+
+    const observer = (index: number) => new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const target = entry.target as HTMLDivElement;
+        this.cardList[index] = {
+          isVisible: entry.isIntersecting,
+          element: target,
+        };
+      }, {
+        threshold: [0, 1],
+      });
+    });
+    // それぞれのカード要素の表示/非表示を監視
+    Array.from(cardsWrapperRef.children).forEach((ele, index) => {
+      observer(index).observe(ele);
+    });
+
+    // 100ms 毎に横方向のスクロールイベントを監視
+    cardsWrapperRef.addEventListener('scroll', debounce((e) => {
+      const element = e.target as HTMLDivElement;
+      this.scrollLeft = Math.ceil(element.scrollLeft);
+    }, 100));
+  },
+
+  methods: {
+    onLeftButtonClicked() {
+      const leftSideElementRight = this.cardList
+        .find((element) => element.isVisible)!.element.getBoundingClientRect().right;
+      const cardsWrapperRef = this.$refs.cardsWrapper as HTMLDivElement;
+      // ラッパーの右端の絶対位置 - 余白
+      const rightSideEdgeRight = cardsWrapperRef
+        .getBoundingClientRect().right - CARDS_WRAPPER_MARGIN;
+
+      cardsWrapperRef.scrollBy({
+        left: leftSideElementRight - rightSideEdgeRight,
+        behavior: 'smooth',
+      });
+    },
+    onRightButtonClicked() {
+      const rightSideElementLeft = () => {
+        const { length } = this.cardList;
+        let previousIsVisible: boolean = false;
+        for (let i = 0; i < length; i += 1) {
+          // true -> false に変わるところ
+          if (previousIsVisible && !this.cardList[i].isVisible) {
+            return this.cardList[i - 1].element.getBoundingClientRect().left;
+          }
+          previousIsVisible = this.cardList[i].isVisible;
+        }
+        return this.cardList[length - 1].element.getBoundingClientRect().left;
+      };
+
+      const cardsWrapperRef = this.$refs.cardsWrapper as HTMLDivElement;
+      // ラッパーの左端の絶対位置 ⁺ 余白
+      const leftSideEdgeLeft = cardsWrapperRef
+        .getBoundingClientRect().left + CARDS_WRAPPER_MARGIN;
+      cardsWrapperRef.scrollBy({
+        left: rightSideElementLeft() - leftSideEdgeLeft,
+        behavior: 'smooth',
+      });
+    },
+  },
+});
 </script>
 
 <style lang="scss" module>
@@ -57,6 +166,7 @@ $gradation-width: 24px;
 .CardsWrapper {
   margin: 0 ($total-side-padding - $gradation-width);
   position: relative;
+  scroll-behavior: smooth;
   &__icon {
     // 親に position: relative 以外が指定されている場合は親要素が基準になる
     position: absolute;
