c9404bf84caa085c974d1cf4eff93380f82eb422
nokazn <baleariclemon@gmail.com>
2020-05-27 21:55:21 +0900

    fix error page

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -1,42 +1,55 @@
 <template>
-  <v-app
-    dark
-    :class="$style.ErrorPage">
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      トップへ戻る
-    </NuxtLink>
-  </v-app>
+  <page>
+    <v-app
+      dark
+      :class="$style.ErrorPage">
+      <h1>
+        {{ errorMessage }}
+      </h1>
+      <NuxtLink to="/">
+        トップへ戻る
+      </NuxtLink>
+    </v-app>
+  </page>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { NuxtError } from '@nuxt/types';
+
+import Page from '~/components/globals/Page.vue';
+
+type Data = {
+  errorMessage: string
+}
+
+export default Vue.extend({
   layout: 'empty',
+  components: {
+    Page,
+  },
+
   props: {
     error: {
-      type: Object,
+      type: Object as PropType<NuxtError>,
       default: null,
     },
   },
-  data() {
+
+  data(): Data {
+    const errorMessage = this.error.statusCode === 404
+      ? '404 Not Found'
+      : 'An error occurred';
+
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+      errorMessage,
     };
   },
-  head() {
-    const title = this.error.statusCode === 404
-      ? this.pageNotFound
-      : this.otherError;
 
-    return { title };
+  head(): { title: string } {
+    return { title: this.errorMessage };
   },
-};
+});
 </script>
 
 <style lang="scss" module>
