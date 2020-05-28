b3d0fbc0e14cae695f9e59e2197a8b7eba4f8275
nokazn <baleariclemon@gmail.com>
2020-05-28 13:01:29 +0900

    change NavigationDrawer button

--- client/variables.ts
+++ client/variables.ts
@@ -4,7 +4,7 @@ export const BACKGROUND_RGB_LIST = [12, 12, 12];
 export const BACKGROUND_COLOR = '#121212';
 export const HEADER_BACKGROUND_COLOR = '#121212';
 export const FOOTER_BACKGROUND_COLOR = '#323232';
-export const NAVIGATION_DRAWER_BACKGROUND_COLOR = '#242424';
+export const NAVIGATION_DRAWER_BACKGROUND_COLOR = '#161616';
 
 export const REPEAT_STATE_LIST: SpotifyAPI.RepeatState[] = [
   'off',
