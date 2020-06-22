5f007fcf6dfd249c4fb6598d12c4fac2911a94c5
nokazn <baleariclemon@gmail.com>
2020-06-22 21:25:54 +0900

    add type definition

--- client/scripts/localPlugins/_artistId/getIsFollowing.ts
+++ client/scripts/localPlugins/_artistId/getIsFollowing.ts
@@ -1,6 +1,6 @@
 import { Context } from '@nuxt/types';
 
-export const getIsFollowing = async ({ app, params }: Context) => {
+export const getIsFollowing = async ({ app, params }: Context): Promise<boolean> => {
   const [isFollowing] = await app.$spotify.following.checkUserFollowed({
     type: 'artist',
     artistIdList: [params.artistId],
