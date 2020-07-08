14d46b10558223d90968b051171a2e06c2b0b90c
nokazn <baleariclemon@gmail.com>
2020-07-08 22:15:42 +0900

    rename

--- client/components/containers/menu/ArtistMenu.vue
+++ client/components/containers/menu/ArtistMenu.vue
@@ -32,7 +32,7 @@ export default Vue.extend({
 
   computed: {
     menuItemLists(): MenuItem[][] {
-      const saveTrack = () => {
+      const followArtist = () => {
         const params = [this.artist.id];
 
         return this.isFollowing
@@ -64,7 +64,7 @@ export default Vue.extend({
       };
 
       return [
-        [saveTrack()],
+        [followArtist()],
         [share()],
       ];
     },
