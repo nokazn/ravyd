683bbc7e5e340484d0d64718e089840eefdaaca9
nokazn <baleariclemon@gmail.com>
2020-07-10 14:44:22 +0900

    fix #198: inccorect addedAt

--- client/scripts/converter/convertAddedAt.ts
+++ client/scripts/converter/convertAddedAt.ts
@@ -12,12 +12,12 @@ export const convertAddedAt = (addedAt: string): App.AddedAtInfo => {
   // 1970-01-01-T00:00:00Z とかを無効にするため、2000年以前のものは無視
   const isTooOld = moment.valueOf() <= new Date(2000, 0, 1).getMilliseconds();
   const text = overTwoWeeksAgo
-    ? moment.format('YYYY/M/d')
+    ? moment.format('YYYY/M/D')
     : moment.fromNow();
 
   return {
     text: isTooOld ? undefined : text,
-    title: moment.format('YYYY/M/d H:mm'),
+    title: moment.format('YYYY/M/D H:mm'),
     dateTime: addedAt,
   };
 };
