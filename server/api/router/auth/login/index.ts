093d4ccf57e12e8f6ec9fdb0125de1dfe4c4c00f
nokazn <baleariclemon@gmail.com>
2020-07-20 13:23:09 +0900

    quit regenerating session on login

--- server/api/router/auth/login/index.ts
+++ server/api/router/auth/login/index.ts
@@ -16,7 +16,7 @@ type ResponseBody = {
   url: string
 }
 
-export const login = (req: Request<RequestParams>, res: Response<ResponseBody>) => {
+export const login = async (req: Request<RequestParams>, res: Response<ResponseBody>) => {
   if (req.session == null) {
     return res.status(401).send({
       accessToken: undefined,
@@ -25,88 +25,73 @@ export const login = (req: Request<RequestParams>, res: Response<ResponseBody>) 
     });
   }
 
-
-  return req.session.regenerate(async (err) => {
-    if (err != null) {
-      return console.error({ err });
-    }
-
-    if (req.session == null) {
-      return res.status(401).send({
+  const currentToken: SpotifyAPI.Auth.TokenResponseData | undefined = req.session?.token;
+  // リフレッシュトークンが存在していた場合はそれを更新
+  if (currentToken?.refresh_token != null) {
+    const token = await refreshAccessToken(currentToken.refresh_token);
+    if (token == null) {
+      return res.status(400).send({
         accessToken: undefined,
         expireIn: 0,
         message: 'トークンを更新できませんでした。',
       });
     }
 
-    const currentToken: SpotifyAPI.Auth.TokenResponseData | undefined = req.session?.token;
-    // リフレッシュトークンが存在していた場合はそれを更新
-    if (currentToken?.refresh_token != null) {
-      const token = await refreshAccessToken(currentToken.refresh_token);
-      if (token == null) {
-        return res.status(400).send({
-          accessToken: undefined,
-          expireIn: 0,
-          message: 'トークンを更新できませんでした。',
-        });
-      }
+    req.session.token = {
+      ...req.session.token,
+      ...token,
+    };
 
-      req.session.token = {
-        ...req.session.token,
-        ...token,
-      };
-
-      return res.send({
-        accessToken: token.access_token,
-        expireIn: TOKEN_EXPIRE_IN,
-      });
-    }
-
-    const clientId = process.env.SPOTIFY_CLIENT_ID;
-    const spotifyBaseUrl = process.env.BASE_URL;
-    if (clientId == null || spotifyBaseUrl == null) {
-      console.error(
-        '環境変数が設定されていません。',
-        JSON.stringify(process.env, undefined, 2),
-      );
-      return res.status(500).send({
-        message: 'エラーが発生しました。',
-        accessToken: undefined,
-        expireIn: 0,
-      });
-    }
-
-    // 認可用のページの URL を返す
-    const baseUrl = 'https://accounts.spotify.com/authorize';
-    const state = generateRandomString();
-    const scope = [
-      'user-read-private',
-      'user-read-email',
-      'user-read-playback-state',
-      'streaming',
-      'user-modify-playback-state',
-      'playlist-modify-public',
-      'playlist-modify-private',
-      'ugc-image-upload',
-      'user-library-read',
-      'user-library-modify',
-      'user-top-read',
-      'user-read-playback-position',
-      'user-read-currently-playing',
-      'user-read-recently-played',
-      'playlist-read-private',
-      'user-follow-read',
-      'user-follow-modify',
-    ].join(' ');
-
-    const url = createUrl(baseUrl, {
-      client_id: clientId,
-      response_type: 'code',
-      redirect_uri: `${spotifyBaseUrl}/login/callback`,
-      state,
-      scope,
+    return res.send({
+      accessToken: token.access_token,
+      expireIn: TOKEN_EXPIRE_IN,
     });
+  }
 
-    return res.send({ url });
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const spotifyBaseUrl = process.env.BASE_URL;
+  if (clientId == null || spotifyBaseUrl == null) {
+    console.error(
+      '環境変数が設定されていません。',
+      JSON.stringify(process.env, undefined, 2),
+    );
+    return res.status(500).send({
+      message: 'エラーが発生しました。',
+      accessToken: undefined,
+      expireIn: 0,
+    });
+  }
+
+  // 認可用のページの URL を返す
+  const baseUrl = 'https://accounts.spotify.com/authorize';
+  const state = generateRandomString();
+  const scope = [
+    'user-read-private',
+    'user-read-email',
+    'user-read-playback-state',
+    'streaming',
+    'user-modify-playback-state',
+    'playlist-modify-public',
+    'playlist-modify-private',
+    'ugc-image-upload',
+    'user-library-read',
+    'user-library-modify',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'playlist-read-private',
+    'user-follow-read',
+    'user-follow-modify',
+  ].join(' ');
+
+  const url = createUrl(baseUrl, {
+    client_id: clientId,
+    response_type: 'code',
+    redirect_uri: `${spotifyBaseUrl}/login/callback`,
+    state,
+    scope,
   });
+
+  return res.send({ url });
 };
