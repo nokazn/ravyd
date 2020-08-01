ed4d6463719e18ddd79ca15c07c068e324582ab5
nokazn <nokazn@gmail.com>
2020-08-01 17:03:04 +0900

    move ReleaseMenu

--- client/pages/releases/_releaseId.vue
+++ client/pages/releases/_releaseId.vue
@@ -153,7 +153,7 @@ import HashTags from '~/components/parts/chip/HashTags.vue';
 import ArtistNames from '~/components/parts/text/ArtistNames.vue';
 import ContextMediaButton, { On as OnMediaButton } from '~/components/parts/button/ContextMediaButton.vue';
 import FavoriteButton, { On as OnFavorite } from '~/components/parts/button/FavoriteButton.vue';
-import ReleaseMenu, { On as OnMenu } from '~/components/containers/menu/ReleaseMenu.vue';
+import ReleaseMenu, { On as OnMenu } from '~/components/parts/menu/ReleaseMenu.vue';
 import ReleaseDate from '~/components/parts/text/ReleaseDate.vue';
 import ReleaseTotalTracks from '~/components/parts/text/ReleaseTotalTracks.vue';
 import ReleaseDuration from '~/components/parts/text/ReleaseDuration.vue';
