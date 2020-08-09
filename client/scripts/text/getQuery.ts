a1a0cc6e0a0301e7e0bb8e717bfe65d69a57dcf6
nokazn <nokazn@gmail.com>
2020-08-09 17:13:21 +0900

    fix getQuery

--- client/scripts/text/getQuery.ts
+++ client/scripts/text/getQuery.ts
@@ -1,8 +1,9 @@
 import { Route } from 'vue-router';
 
 export const getQuery = (query: Route['query'], key: string): string | undefined => {
+  // @todo param は Nullable
   const param = query[key];
-  if (typeof param === 'string') return param;
+  if (param == null || typeof param === 'string') return param;
 
   const filteredParams = param.filter((val) => val != null) as string[];
   return filteredParams[0];
