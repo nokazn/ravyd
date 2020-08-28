57cfeae28f0c866d71e6dc53f86353f71d64ad44
nokazn <nokazn@gmail.com>
2020-08-29 00:00:32 +0900

    fix login

--- client/plugins/server/endpoints/auth/login.ts
+++ client/plugins/server/endpoints/auth/login.ts
@@ -4,8 +4,8 @@ import { ServerAPI } from '~~/types';
 export const login = (context: Context) => {
   const { app } = context;
 
-  return (): Promise<ServerAPI.Auth.Token | undefined> => {
-    const request = app.$serverApi.$get('/auth/login')
+  return (): Promise<ServerAPI.Auth.Login | undefined> => {
+    const request = app.$serverApi.$post('/auth/login')
       .catch((err: Error) => {
         console.error({ err });
         return undefined;
