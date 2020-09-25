17124ffddd4842d3bdb3e23cca10346e3eeee08b
nokazn <nokazn@gmail.com>
2020-09-25 13:16:37 +0900

    remove unnecessary duplicated lint & update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,9 +30,61 @@ const nuxtConfig: NuxtConfig = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  css: [
-    './assets/global.scss',
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxt/typescript-build',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/vuetify',
   ],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    'portal-vue/nuxt',
+  ],
+  env: {
+    BASE_URL: process.env.BASE_URL || 'https://127.0.0.1:3000',
+  },
+  server: {
+    https: process.env.NODE_ENV === 'development'
+      ? {
+        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+      }
+      : undefined,
+  },
+  build: {
+    analyze: process.env.NODE_ENV === 'development',
+    babel: {
+      presets({ isServer }) {
+        return [
+          // corejs のバージョンの競合を防ぐ
+          [
+            '@nuxt/babel-preset-app',
+            {
+              loose: true,
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 },
+            },
+          ],
+        ];
+      },
+    },
+    extend(_config) {
+      // worker-loader を読み込む
+      // config.module = config.module ?? { rules: [] };
+      // https://github.com/nuxt/nuxt.js/pull/3480#issuecomment-404150387
+      // config.output = config.output ?? {};
+      // config.output.globalObject = 'this';
+      // if (isClient) {
+      //   config.module.rules.push({
+      //     test: /bundle\.worker\.js$/,
+      //     loader: 'worker-loader',
+      //   });
+      // }
+    },
+  },
   serverMiddleware: [
     '~~/server/app/',
   ],
@@ -51,23 +103,10 @@ const nuxtConfig: NuxtConfig = {
   router: {
     middleware: 'auth',
   },
-  env: {
-    BASE_URL: process.env.BASE_URL || 'https://127.0.0.1:3000',
-  },
+  css: [
+    './assets/global.scss',
+  ],
   loading: { color: '#fff' },
-  buildModules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/dotenv',
-    '@nuxt/typescript-build',
-    '@nuxtjs/vuetify',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/stylelint-module',
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    'portal-vue/nuxt',
-  ],
   axios: {
     baseURL: process.env.BASE_URL,
     browserBaseURL: 'https://api.spotify.com/v1',
@@ -82,6 +121,21 @@ const nuxtConfig: NuxtConfig = {
   },
   dotenv: {
     filename: process.env.NODE_ENV === 'production' ? './.env.prod' : './.env.dev',
+  },
+  stylelint: {
+    configFile: './.stylelintrc.js',
+    fix: true,
+  },
+  styleResources: {
+    scss: ['~/assets/variables.scss'],
+  },
+  typescript: {
+    // type-check & lint
+    typeCheck: {
+      eslint: {
+        files: './client/**/*.{ts,js,vue}',
+      },
+    },
   },
   vuetify: {
     customVariables: ['~/assets/vuetify.scss'],
@@ -108,69 +162,6 @@ const nuxtConfig: NuxtConfig = {
           subtext: colors.grey.lighten1,
           inactive: colors.grey.darken1,
         },
-      },
-    },
-  },
-  styleResources: {
-    scss: ['~/assets/variables.scss'],
-  },
-  stylelint: {
-    configFile: './.stylelintrc.js',
-    fix: true,
-  },
-  server: {
-    https: process.env.NODE_ENV === 'development'
-      ? {
-        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-      }
-      : undefined,
-  },
-  build: {
-    analyze: true,
-    babel: {
-      presets({ isServer }) {
-        return [
-          // corejs のバージョンの競合を防ぐ
-          [
-            '@nuxt/babel-preset-app',
-            {
-              loose: true,
-              buildTarget: isServer ? 'server' : 'client',
-              corejs: { version: 3 },
-            },
-          ],
-        ];
-      },
-    },
-    extend(config, { isDev, isClient }) {
-      config.module = config.module ?? { rules: [] };
-
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        });
-      }
-
-      // https://github.com/nuxt/nuxt.js/pull/3480#issuecomment-404150387
-      config.output = config.output ?? {};
-      config.output.globalObject = 'this';
-
-      if (isClient) {
-        config.module.rules.push({
-          test: /bundle\.worker\.js$/,
-          loader: 'worker-loader',
-        });
-      }
-    },
-  },
-  typescript: {
-    typeCheck: {
-      eslint: {
-        files: './client/**/*.{ts,js,vue}',
       },
     },
   },
