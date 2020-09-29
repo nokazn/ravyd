d56bc39dede9d15e32eca2e897b99df0c9cee2dd
nokazn <nokazn@gmail.com>
2020-09-29 10:01:00 +0900

    adjust for mobile in Root page

--- client/plugins/observable/window.ts
+++ client/plugins/observable/window.ts
@@ -1,7 +1,7 @@
 import type { Plugin } from '@nuxt/types';
 import { $window } from '~/observable/window';
 
-export type { $Window } from '~/observable/window';
+export type { $Window, DeviceType } from '~/observable/window';
 
 const injector: Plugin = (_, inject) => {
   inject('window', $window);
