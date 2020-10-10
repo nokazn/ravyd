1bb31a52fa6aadca2d10fc98f01a598410b73487
nokazn <nokazn@gmail.com>
2020-10-11 00:02:02 +0900

    fix invalid prop

--- client/components/parts/list/TwoColumnsListCard.vue
+++ client/components/parts/list/TwoColumnsListCard.vue
@@ -1,36 +1,37 @@
 <template>
-  <v-list
-    rounded="lg"
-    :elevation="12"
-    :color="$constant.CARD_BACKGROUND_COLOR"
-    :class="$style.TwoColumnsList"
-  >
-    <template
-      v-for="(item, index) in items"
+  <v-card rounded="lg">
+    <v-list
+      :elevation="12"
+      :color="$constant.CARD_BACKGROUND_COLOR"
+      :class="$style.TwoColumnsList"
     >
-      <v-list-item
-        :key="item.title"
-        :class="$style.TwoColumnsList__item"
+      <template
+        v-for="(item, index) in items"
       >
-        <v-list-item-content>
-          <v-list-item-title :class="$style.TwoColumnsList__content">
-            <div class="subtext--text">
-              {{ item.title }}
-            </div>
+        <v-list-item
+          :key="item.title"
+          :class="$style.TwoColumnsList__item"
+        >
+          <v-list-item-content>
+            <v-list-item-title :class="$style.TwoColumnsList__content">
+              <div class="subtext--text">
+                {{ item.title }}
+              </div>
 
-            <div>
-              {{ item.value }}
-            </div>
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+              <div>
+                {{ item.value }}
+              </div>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-divider
-        v-show="items.length !== index + 1"
-        :key="index"
-      />
-    </template>
-  </v-list>
+        <v-divider
+          v-show="items.length !== index + 1"
+          :key="index"
+        />
+      </template>
+    </v-list>
+  </v-card>
 </template>
 
 <script lang="ts">
