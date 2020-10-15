029e0502e2ed4665f37ed6024ad60dc246eedb08
nokazn <nokazn@gmail.com>
2020-10-15 17:15:46 +0900

    refactor: refresh

--- client/services/server/auth/refresh.ts
+++ client/services/server/auth/refresh.ts
@@ -1,18 +1,15 @@
 import { Context } from '@nuxt/types';
-import { AxiosResponse, AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { ServerAPI } from '~~/types';
 
 export const refresh = (context: Context) => {
   const { app } = context;
 
-  // status も見たいので $serverApi.$post 出なく、$serverApi.post で呼び出してる
-  return (accessToken: string): Promise<AxiosResponse<ServerAPI.Auth.Token> | undefined> => {
-    const request = app.$serverApi.post('/auth/refresh', { accessToken })
+  return (accessToken: string): Promise<ServerAPI.Auth.Token> => {
+    return app.$serverApi.$post('/auth/refresh', { accessToken })
       .catch((err: AxiosError<ServerAPI.Auth.Token>) => {
         console.error({ err });
-        return err.response;
+        throw err;
       });
-
-    return request;
   };
 };
