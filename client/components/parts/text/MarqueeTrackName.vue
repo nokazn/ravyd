94307d3c0bd28f931ebaac1e48649518bf93aa49
nokazn <nokazn@gmail.com>
2020-10-24 03:37:19 +0900

    refactor: switch to composition-api in MarqueeText

--- client/components/parts/text/MarqueeTrackName.vue
+++ client/components/parts/text/MarqueeTrackName.vue
@@ -3,44 +3,38 @@
     :title="name"
     :class="[$style.TrackName, 'g-text-gradation']"
   >
-    <nuxt-link
-      v-if="trackPath != null"
-      :id="TRACK_NAME_LINK"
-      :to="trackPath"
-      :style="marqueeStyles"
-      @mouseover.native="onHovered"
-    >
-      {{ name }}
-    </nuxt-link>
     <span
-      v-else
-      :id="TRACK_NAME_LINK"
+      ref="MARQUEE_TEXT_REF"
       :style="marqueeStyles"
       @mouseover="onHovered"
     >
-      {{ name }}
+      <nuxt-link
+        v-if="trackPath != null"
+        :to="trackPath"
+      >
+        {{ name }}
+      </nuxt-link>
+      <span v-else>
+        {{ name }}
+      </span>
     </span>
   </div>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue';
+import Vue from 'vue';
+import {
+  defineComponent,
+  computed,
+  ref,
+  watch,
+  PropType,
+} from '@vue/composition-api';
 import { RawLocation } from 'vue-router';
-
-import { sleep } from '~~/utils/sleep';
+import { useMarqueeText } from '~/services/use/style';
 import { SpotifyAPI } from '~~/types';
 
-const TRACK_NAME_LINK = 'TRACK_NAME_LINK';
-
-type Data = {
-  isHovered: boolean
-  animationTimeoutId: ReturnType<typeof setTimeout> | undefined
-  parentWidth: number | undefined
-  linkWidth: number | undefined
-  TRACK_NAME_LINK: string
-};
-
-export default Vue.extend({
+export default defineComponent({
   props: {
     id: {
       type: String,
@@ -60,86 +54,40 @@ export default Vue.extend({
     },
   },
 
-  data(): Data {
-    return {
-      isHovered: false,
-      animationTimeoutId: undefined,
-      parentWidth: undefined,
-      linkWidth: undefined,
-      TRACK_NAME_LINK,
-    };
-  },
-
-  computed: {
-    trackPath(): RawLocation | undefined {
-      switch (this.type) {
+  setup(props) {
+    const MARQUEE_TEXT_REF = ref<Vue>();
+    const trackPath = computed<RawLocation | undefined>(() => {
+      switch (props.type) {
         case 'track':
           return {
-            path: `/releases/${this.releaseId}`,
-            query: { track: this.id },
+            path: `/releases/${props.releaseId}`,
+            query: { track: props.id },
           };
         case 'episode':
-          return `/episodes/${this.id}`;
+          return `/episodes/${props.id}`;
         default:
           return undefined;
       }
-    },
-    marqueeSeconds(): number | undefined {
-      if (!this.isHovered || this.parentWidth == null || this.linkWidth == null) return undefined;
-      if (this.linkWidth < this.parentWidth * 0.95) return undefined;
+    });
 
-      const widthPerSeconds = 30;
-      return Math.ceil((this.linkWidth / widthPerSeconds));
-    },
-    marqueeStyles(): { animation: string } | undefined {
-      return this.marqueeSeconds != null
-        ? { animation: `marquee ${this.marqueeSeconds}s linear 1` }
-        : undefined;
-    },
-  },
+    const {
+      marqueeStyles,
+      calculateWidth,
+      onHovered,
+      resetTimer,
+    } = useMarqueeText(MARQUEE_TEXT_REF);
 
-  watch: {
-    releaseId(): void {
-      this.clearTimeout();
-    },
-  },
+    watch(ref(props.releaseId), () => {
+      resetTimer();
+      calculateWidth();
+    });
 
-  methods: {
-    calculateWidth() {
-      const linkEle = document.getElementById(TRACK_NAME_LINK);
-      if (linkEle == null) {
-        console.error(`Not Found Element of which id is "${TRACK_NAME_LINK}"`);
-        return;
-      }
-
-      this.linkWidth = linkEle.clientWidth;
-      const { parentElement } = linkEle;
-      this.parentWidth = parentElement?.clientWidth;
-    },
-    async onHovered() {
-      this.calculateWidth();
-      // 既にアニメーションがスタートしてる場合はキャンセル
-      if (this.isHovered) return;
-
-      // ホバーしてから0.5秒後にアニメーションを開始
-      const extraMillSeconds = 500;
-      await sleep(extraMillSeconds);
-      this.isHovered = true;
-      if (this.marqueeSeconds == null) {
-        this.isHovered = false;
-        return;
-      }
-
-      // アニメーション終了後に再度アニメーションを受け付けられる状態にする
-      this.animationTimeoutId = setTimeout(() => {
-        this.isHovered = false;
-      }, this.marqueeSeconds * 1000 + extraMillSeconds);
-    },
-    clearTimeout() {
-      if (this.animationTimeoutId != null) clearTimeout(this.animationTimeoutId);
-      this.animationTimeoutId = undefined;
-      this.isHovered = false;
-    },
+    return {
+      MARQUEE_TEXT_REF,
+      trackPath,
+      marqueeStyles,
+      onHovered,
+    };
   },
 });
 </script>
