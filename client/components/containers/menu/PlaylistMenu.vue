2dc080c5da5a598723d3dbdc6c1146ab15fc860b
nokazn <nokazn@gmail.com>
2020-11-08 01:28:45 +0900

    fix: toggle following-playlists state

--- client/components/containers/menu/PlaylistMenu.vue
+++ client/components/containers/menu/PlaylistMenu.vue
@@ -136,7 +136,7 @@ export default defineComponent({
       const type = 'custom';
       const isOwnPlaylist = props.playlist.owner.id === root.$getters()['auth/userId'];
       const handler = () => {
-        emit(ON_FOLLOW_MENU_CLICKED, props.following);
+        emit(ON_FOLLOW_MENU_CLICKED, !props.following);
       };
       return props.following
         ? {
