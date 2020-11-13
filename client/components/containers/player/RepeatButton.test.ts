9e0a2aa42c6cc2c6f43e8a3e36a7616cd372ad81
nokazn <nokazn@gmail.com>
2020-11-13 11:30:48 +0900

    switch to composition-api & add tests in RepeatButton

---
+++ client/components/containers/player/RepeatButton.test.ts
@@ -0,0 +1,69 @@
+import { mount } from '@vue/test-utils';
+import { options, mocks } from '~/tests/mocks/mount';
+import RepeatButton from './RepeatButton.vue';
+import CircleButton from '~/components/parts/button/CircleButton.vue';
+
+const CLICK = 'click';
+
+const $stateMock = (repeatMode: 0 | 1 | 2) => {
+  return jest.fn().mockReturnValue({
+    playback: {
+      repeatMode,
+    },
+  });
+};
+const $gettersMock = (isDisallowed: boolean) => {
+  return jest.fn().mockReturnValue({
+    'playback/isDisallowed': () => isDisallowed,
+  });
+};
+const $dispatchMock = jest.fn().mockResolvedValue(undefined);
+
+const factory = (mode: 0 | 1 | 2, isDisallowed: boolean = false) => {
+  return mount(RepeatButton, {
+    ...options,
+    mocks: {
+      ...mocks,
+      $state: $stateMock(mode),
+      $getters: $gettersMock(isDisallowed),
+      $dispatch: $dispatchMock,
+    },
+  });
+};
+
+describe('RepeatButton', () => {
+  it('normal mode', async () => {
+    const wrapper = factory(0);
+    const button = wrapper.findComponent(CircleButton);
+    expect(button.attributes().title).toBe('リピート再生');
+    expect(button.props().iconColor).toBe('inactive');
+    expect(wrapper.find('.v-icon.mdi-repeat-off').exists()).toBe(true);
+  });
+
+  it('repeat mode', async () => {
+    const wrapper = factory(1);
+    const button = wrapper.findComponent(CircleButton);
+    expect(button.attributes().title).toBe('曲をリピート再生');
+    expect(button.props().iconColor).toBe('active-icon');
+    expect(wrapper.find('.v-icon.mdi-repeat').exists()).toBe(true);
+  });
+
+  it('repeat-once mode', async () => {
+    const wrapper = factory(2);
+    const button = wrapper.findComponent(CircleButton);
+    expect(button.attributes().title).toBe('リピート再生しない');
+    expect(button.props().iconColor).toBe('active-icon');
+    expect(wrapper.find('.v-icon.mdi-repeat-once').exists()).toBe(true);
+  });
+
+  it('disabled', async () => {
+    const wrapper = factory(0, true);
+    expect(wrapper.findComponent(CircleButton).props().disabled).toBe(true);
+  });
+
+  it('click button', async () => {
+    const wrapper = factory(0);
+    await wrapper.findComponent(CircleButton).trigger(CLICK);
+    expect($dispatchMock).toHaveBeenCalledWith('playback/repeat');
+  });
+});
