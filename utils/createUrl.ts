cdd271d978a35e90a08f8d9ca8619f748f692de0
nokazn <nokazn@gmail.com>
2020-11-28 20:24:36 +0900

    tsonfig & server script

--- utils/createUrl.ts
+++ utils/createUrl.ts
@@ -1,8 +1,7 @@
-type UrlParams = {
-  [key: string]: string;
-}
-
-export const createUrl = (baseUrl: string, params: UrlParams): string => {
+export const createUrl = (
+  baseUrl: string,
+  params: Record<string, string>,
+): string => {
   const url = new URL(baseUrl);
   Object.entries(params).forEach(([key, value]) => {
     url.searchParams.append(key, value);
