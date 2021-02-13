b2055755e4cb054d836473beaeb117a1772e276f
nokazn <nokazn@gmail.com>
2021-02-13 23:07:51 +0900

    remove serverMiddleware

--- serverMiddleware/config/constants.ts
+++
@@ -1,7 +0,0 @@
-export const SPOTIFY_TOKEN_BASE_URL = 'https://accounts.spotify.com/api/token';
-export const SPOTIFY_AUTHORIZE_BASE_URL = 'https://accounts.spotify.com/authorize';
-
-export const CSRF_STATE_COOKIE_KEY = 'csrfState';
-
-// // 20分でトークンを更新させる
-export const TOKEN_EXPIRE_IN = 1000 * 60 * 20;
--- serverMiddleware/middleware/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './cookieParser';
-export * from './session';
